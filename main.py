60e7178f2462e68af948788ccb8942e5140ebae2
HonFi <74440672+Honfi555@users.noreply.github.com>
2024-07-02 16:02:04 +1100

    add finding traveled distance and time

--- main.py
+++ main.py
@@ -3,50 +3,52 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 
 
-departure_station = '654909'
-operation_station = '828501'
-cargo = '20100'
+WAGON: str = 'Цистерны для нефти'
+DEPARTURE_STATION: str = '654909'
+OPERATION_STATION: str = '828501'
+CARGO: str = '20100'
 
 
-def main() -> None:
+def parse_data(page_source: str) -> list[int]:
+    soup: BeautifulSoup = BeautifulSoup(page_source, 'html.parser')
+    result: list[int] = []
+    for value in soup.find(class_='form-result').find_all(class_='form-result__col__value')[1:3]:
+        result += value.text.split(' ')[0]
+    return result
+
+
+def enter_param() -> str:
     driver = webdriver.Chrome()
     driver.get('https://spimex.com/markets/oil_products/rzd/')
 
     driver.implicitly_wait(2)
 
-    user_agreement = driver.find_elements(By.CLASS_NAME, 'terms__footer__btn')[0]
-    user_agreement.click()
+    driver.find_elements(By.CLASS_NAME, 'terms__footer__btn')[0].click()
+    driver.find_element(By.CLASS_NAME, 'cookie-agree__button').click()
 
-    organization_info = driver.find_element(By.CLASS_NAME, 'cookie-agree__button')
-    organization_info.click()
+    driver.find_element(By.NAME, 'wagon').send_keys(WAGON + Keys.RETURN)
 
-    wagon_elem = driver.find_element(By.NAME, 'wagon')
-    wagon_elem.send_keys('Цистерны для нефти' + Keys.RETURN)
-
-    departure_station_elem = driver.find_element(By.NAME, 'station_from')
-    departure_station_elem.send_keys(departure_station)
-
+    driver.find_element(By.NAME, 'station_from').send_keys(DEPARTURE_STATION)
     driver.find_element(By.CLASS_NAME, 'form__col-1-2').find_element(By.TAG_NAME, 'li').click()
 
-    operation_station_elem = driver.find_element(By.NAME, 'station_to')
-    operation_station_elem.send_keys(operation_station)
-
+    driver.find_element(By.NAME, 'station_to').send_keys(OPERATION_STATION)
     driver.find_elements(By.CLASS_NAME, 'form__col-1-2')[1].find_element(By.TAG_NAME, 'li').click()
 
-    cargo_elem = driver.find_element(By.NAME, 'product')
-    cargo_elem.send_keys(cargo)
-
+    driver.find_element(By.NAME, 'product').send_keys(CARGO)
     time.sleep(1)
-
     driver.find_element(By.CLASS_NAME, 'form__col-1-1').find_element(By.TAG_NAME, 'li').click()
 
     driver.find_element(By.CLASS_NAME, 'form__submit').click()
 
-    time.sleep(10)
+    time.sleep(1)
     driver.close()
+    return driver.page_source
 
 
 if __name__ == '__main__':
-    main()
+    traveled_distance, traveled_time = parse_data(enter_param())
+    print(traveled_distance, traveled_time, sep=' ')
+    
