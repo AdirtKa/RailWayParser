de4ede3d75dd84698ec8587eaa3cac8e3d7a8b26
HonFi <74440672+Honfi555@users.noreply.github.com>
2024-07-05 15:18:45 +1100

    change browser for parsing to Microsoft Edge

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from database import prepare_data
 
 
 def main() -> pd.DataFrame:
-    # df = prepare_data(0)
-    df = pd.DataFrame([[datetime.datetime(2024, 3, 2, 1, 56, 00),
-                       datetime.datetime(2024, 3, 10, 1, 56, 00),
-                       'БЕНЗИН (654909)', 'КУРГАН (828501)']],
-                      columns=['start_datetime', 'operation_datetime', 'departure_station', 'operation_station'])
+    df = prepare_data(5)
+    # df = pd.DataFrame([[datetime.datetime(2024, 3, 2, 1, 56, 00),
+    #                    datetime.datetime(2024, 3, 10, 1, 56, 00),
+    #                    'БЕНЗИН (654909)', 'КУРГАН (828501)']],
+    #                   columns=['start_datetime', 'operation_datetime', 'departure_station', 'operation_station'])
     df_travel = pd.DataFrame([], columns=['traveled_distance', 'traveled_days'])
 
     for index, row in df.iterrows():
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), f'Row number in processing: {index}', sep=' | ')
         row_data: list[int] = parse_data(enter_param(row['departure_station'], row['operation_station']))
 
         df_row = pd.DataFrame([row_data], columns=['traveled_distance', 'traveled_days'])
@@ -30,4 +31,4 @@ def main() -> pd.DataFrame:
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', 10)
-    print(main())
+    print('Result', main(), sep='\n', end='\n\n')
--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
 import time
 
+from selenium.webdriver.remote.webelement import WebElement
 from selenium import webdriver
+from selenium.webdriver.edge.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
@@ -8,7 +11,13 @@ from bs4 import BeautifulSoup
 
 WAGON: str = 'Цистерны для нефти'
 CARGO: str = '21400'
-# DRIVER_PATH = '/usr/local/bin/chromedriver'
+
+
+def scroll_to_element(element: WebElement, driver: WebDriver) -> WebElement:
+    ActionChains(driver) \
+        .scroll_to_element(element) \
+        .perform()
+    return element
 
 
 def parse_data(page_source: str) -> list[int]:
@@ -16,44 +25,43 @@ def parse_data(page_source: str) -> list[int]:
     result: list[int] = []
     for value in soup.find(class_='form-result').find_all(class_='form-result__col__value')[1:3]:
         numeric_part = ''
-        for potential_num in value.text.split():
+        for potential_num in value.text:
             if potential_num.isnumeric():
                 numeric_part += potential_num
 
-        result.append(int(numeric_part))
+        result.append(int(numeric_part, 10))
 
     return result
 
 
 def enter_param(departure_station: str, operation_station: str) -> str:
-    options = webdriver.ChromeOptions()
+    options = webdriver.EdgeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920x1080')
     options.add_argument('--no-sandbox')
 
-    # service = webdriver.ChromeService(DRIVER_PATH)
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Edge(options=options)
     driver.get('https://spimex.com/markets/oil_products/rzd/')
 
     driver.implicitly_wait(2)
 
-    driver.find_elements(By.CLASS_NAME, 'terms__footer__btn')[0].click()
-    driver.find_element(By.CLASS_NAME, 'cookie-agree__button').click()
+    scroll_to_element(driver.find_elements(By.CLASS_NAME, 'terms__footer__btn')[0], driver).click()
+    scroll_to_element(driver.find_element(By.CLASS_NAME, 'cookie-agree__button'), driver).click()
 
-    driver.find_element(By.NAME, 'wagon').send_keys(WAGON + Keys.RETURN)
+    scroll_to_element(driver.find_element(By.NAME, 'wagon'), driver).send_keys(WAGON + Keys.RETURN)
 
-    driver.find_element(By.NAME, 'station_from').send_keys(departure_station.split(' ')[0])
-    driver.find_element(By.CLASS_NAME, 'form__col-1-2').find_element(By.TAG_NAME, 'li').click()
+    scroll_to_element(driver.find_element(By.NAME, 'station_from'), driver).send_keys(departure_station.split(' ')[0])
+    scroll_to_element(driver.find_element(By.CLASS_NAME, 'form__col-1-2').find_element(By.TAG_NAME, 'li'), driver).click()
 
-    driver.find_element(By.NAME, 'station_to').send_keys(operation_station.split(' ')[0])
+    scroll_to_element(driver.find_element(By.NAME, 'station_to'), driver).send_keys(operation_station.split(' ')[0])
     driver.find_elements(By.CLASS_NAME, 'form__col-1-2')[1].find_element(By.TAG_NAME, 'li').click()
 
-    driver.find_element(By.NAME, 'product').send_keys(CARGO)
+    scroll_to_element(driver.find_element(By.NAME, 'product'), driver).send_keys(CARGO)
     time.sleep(1)
-    driver.find_element(By.CLASS_NAME, 'form__col-1-1').find_element(By.TAG_NAME, 'li').click()
+    scroll_to_element(driver.find_element(By.CLASS_NAME, 'form__col-1-1'), driver).find_element(By.TAG_NAME, 'li').click()
 
-    driver.find_element(By.CLASS_NAME, 'form__submit').click()
+    scroll_to_element(driver.find_element(By.CLASS_NAME, 'form__submit'), driver).click()
 
     time.sleep(1)
     page_source = driver.page_source
@@ -62,4 +70,4 @@ def enter_param(departure_station: str, operation_station: str) -> str:
 
 
 if __name__ == '__main__':
-    print(parse_data(enter_param('654909', '873308')))
+    print('result: ', parse_data(enter_param('654909', '873308')))
