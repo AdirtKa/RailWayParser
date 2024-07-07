d3d122097733a3c053035b93ccab4db9969455be
HonFi <74440672+Honfi555@users.noreply.github.com>
2024-07-07 14:16:20 +1100

    replace main website for data parsing with rzd cargo

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-import datetime
+# import datetime
+import logging
 
 import pandas as pd
 
-from parser import parse_data, enter_param
+import parser
+from logger import create_logger
+from exceptions import UnreadbleStation, NoDataReceived
 from database import prepare_data
 
 
 def main() -> pd.DataFrame:
-    df = prepare_data(5)
-    df_travel = pd.DataFrame([], columns=['traveled_distance', 'traveled_days'])
+    df: pd.DataFrame = prepare_data()
+    # df: pd.DataFrame = pd.DataFrame([[datetime.datetime(2024, 10, 5, 8, 9, 0),
+    #                    datetime.datetime(2024, 11, 5, 6, 9, 0),
+    #                    'БЕНЗИН (654909)', 'КУРГАН (828501)'],
+    #                    [datetime.datetime(2024, 11, 6, 7, 9, 0),
+    #                     datetime.datetime(2024, 11, 20, 0, 0, 0),
+    #                     'БЕНЗИН (654909)', 'ХОЛБОН (948050)']
+    #                    ],
+    #                   columns=['start_datetime', 'operation_datetime', 'departure_station', 'operation_station'])
+    df_travel: pd.DataFrame = pd.DataFrame([], columns=['traveled_distance', 'traveled_days'])
 
+    logger: logging.Logger = create_logger()
     for index, row in df.iterrows():
-        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), f'Row number in processing: {index}', sep=' | ')
-        row_data: list[int] = parse_data(enter_param(row['departure_station'], row['operation_station']))
+        logger.info(f'Row number in processing: {index}\n'
+                    f'\tdeparture_station {row['departure_station']}\n'
+                    f'\toperation_station {row['operation_station']}')
 
-        df_row = pd.DataFrame([row_data], columns=['traveled_distance', 'traveled_days'])
-        df_travel = pd.concat([df_travel, df_row], axis=0)
+        row_data: list[int] = [0, 0]
+        try:
+            row_data: list[int] = parser.enter_param_rzd_cargo(row['departure_station'],
+                                                               row['operation_station'])
+        except (UnreadbleStation, NoDataReceived) as ex:
+            logger.warning(ex.message)
 
-    df_travel = df_travel.reset_index()
+        df_row: pd.DataFrame = pd.DataFrame([row_data], columns=['traveled_distance', 'traveled_days'])
+        df_travel: pd.DataFrame = pd.concat([df_travel, df_row], axis=0)
 
-    result = pd.concat([df_travel,
-                        df[['start_datetime', 'operation_datetime', 'departure_station', 'operation_station']]],
-                       axis=1)
+    df_travel: pd.DataFrame = df_travel.reset_index()
+
+    result: pd.DataFrame = pd.concat([df_travel,
+                                            df[['start_datetime',
+                                                'operation_datetime',
+                                                'departure_station',
+                                                'operation_station']]],
+                                           axis=1)
     return result.loc[:, result.columns != 'index']
 
 
